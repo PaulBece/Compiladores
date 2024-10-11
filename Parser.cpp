9f0b0da372e3a62ce314df1d6c33edc97de2193b
milksake <caceresmariana443@gmail.com>
2024-10-11 14:39:11 -0500

    casi

--- Parser.cpp
+++ Parser.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
 #include "common.h"
 
 std::vector<Token> tokens;
@@ -66,7 +67,7 @@ bool factorPrime2();
 
 bool eval()
 {
-
+    return false;
 }
 
 bool program()
@@ -96,7 +97,7 @@ bool declarationPrime()
 {
     if (check({PARENTHESIS_OPEN}))
         return function();
-    if (check({SEMICOLON, EQUAL}))
+    if (check({SEMICOLON, ASSIGN}))
         return varDecl();
     return false;
 }
@@ -154,8 +155,8 @@ bool varDecl()
 {
     if (check({SEMICOLON}))
         return terminal(SEMICOLON);
-    if (check({EQUAL}))
-        return terminal(EQUAL) && expression() && terminal(SEMICOLON);
+    if (check({ASSIGN}))
+        return terminal(ASSIGN) && expression() && terminal(SEMICOLON);
     return false;
 }
 
@@ -192,4 +193,249 @@ bool statement()
     if (check({BRACE_OPEN}))
         return terminal(BRACE_OPEN) && stmtList() && terminal(BRACE_CLOSE);
     return false;
+}
+
+bool ifStmt()
+{
+    if (check({IF}))
+        return terminal(IF) && terminal(PARENTHESIS_OPEN) && expression() && terminal(PARENTHESIS_CLOSE) && terminal(BRACE_OPEN) && statement() && terminal(BRACE_CLOSE) && ifStmtPrime();
+    return false;
+}
+
+bool ifStmtPrime()
+{
+    if (check({ELSE}))
+        terminal(ELSE) && terminal(BRACE_OPEN) && statement() && terminal(BRACE_CLOSE);
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, BRACE_OPEN, BRACE_CLOSE, INTEGER, BOOLEAN, CHAR, STRING, VOID, SEMICOLON, IF, FOR, RETURN, PRINT, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return true;
+    return false;
+}
+
+bool forStmt()
+{
+    if (check({FOR}))
+        return terminal(FOR) && terminal(PARENTHESIS_OPEN) && exprStmt() && expression() && terminal(SEMICOLON) && exprStmt() && terminal(PARENTHESIS_CLOSE) && statement();
+    return false;
+}
+
+bool returnStmt()
+{
+    if (check({RETURN}))
+        return terminal(RETURN) && expression() && terminal(SEMICOLON);
+    return false;
+}
+
+bool printStmt()
+{
+    if (check({PRINT}))
+        return terminal(PRINT) && terminal(PARENTHESIS_OPEN) && exprList() && terminal(PARENTHESIS_CLOSE) && terminal(SEMICOLON);
+    return false;
+}
+
+bool exprStmt()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return expression() && terminal(SEMICOLON);
+    if (check({SEMICOLON}));
+        return terminal(SEMICOLON);
+    return false;
+}
+
+bool exprList()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return expression() && exprListPrime();
+    return false;
+}
+
+bool exprListPrime()
+{
+    if (check({COMMA}))
+        return terminal(COMMA) && exprList();
+    if (check({PARENTHESIS_CLOSE}))
+        return true;
+    return false;
+}
+
+bool expression()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return orExpr() && expressionPrime();
+    return false;
+}
+
+bool expressionPrime()
+{
+    if (check({ASSIGN}))
+        return terminal(ASSIGN) && orExpr();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON}))
+        return true;
+    return false;
+}
+
+bool orExpr()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return andExpr() && orExprPrime();
+    return false;
+}
+
+bool orExprPrime()
+{
+    if (check({OR}))
+        return terminal(OR) && andExpr() && orExprPrime();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON, ASSIGN}))
+        return true;
+    return false;
+}
+
+bool andExpr()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return eqExpr() && andExprPrime();
+    return false;
+}
+
+bool andExprPrime()
+{
+    if (check({AND}))
+        return terminal(AND) && eqExpr() && andExprPrime();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON, ASSIGN, OR}))
+        return true;
+    return false;
+}
+
+bool eqExpr()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return relExpr() && eqExprPrime();
+    return false;
+}
+
+bool eqExprPrime()
+{
+    if (check({EQUAL}))
+        return terminal(EQUAL) && relExpr() && eqExprPrime();
+    if (check({NOT_EQUAL}))
+        return terminal(NOT_EQUAL) && relExpr() && eqExprPrime();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON, ASSIGN, OR, AND}))
+        return true;
+    return false;
+}
+
+bool relExpr()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return expr() && relExprPrime();
+    return false;
+}
+
+bool relExprPrime()
+{
+    if (check({LESS_THAN}))
+        return terminal(LESS_THAN) && expr() && relExprPrime();
+    if (check({GREATER_THAN}))
+        return terminal(GREATER_THAN) && expr() && relExprPrime();
+    if (check({LESS_EQUAL}))
+        return terminal(LESS_EQUAL) && expr() && relExprPrime();
+    if (check({GREATER_EQUAL}))
+        return terminal(GREATER_EQUAL) && expr() && relExprPrime();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON, ASSIGN, OR, AND, EQUAL, NOT_EQUAL}))
+        return true;
+    return false;
+}
+
+bool expr()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return term() && exprPrime();
+    return false;
+}
+
+bool exprPrime()
+{
+    if (check({PLUS}))
+        return terminal(PLUS) && term() && exprPrime();
+    if (check({MINUS}))
+        return terminal(MINUS) && term() && exprPrime();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON, ASSIGN, OR, AND, EQUAL, NOT_EQUAL, LESS_THAN, GREATER_THAN, LESS_EQUAL, GREATER_EQUAL}))
+        return true;
+    return false;
+}
+
+bool term()
+{
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, MINUS, NOT, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return unary() && termPrime();
+    return false;
+}
+
+bool termPrime()
+{
+    if (check({MULTIPLY}))
+        return terminal(MULTIPLY) && unary() && termPrime();
+    if (check({DIVIDE}))
+        return terminal(DIVIDE) && unary() && termPrime();
+    if (check({MODULO}))
+        return terminal(MODULO) && unary() && termPrime();
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, COMMA, SEMICOLON, ASSIGN, OR, AND, EQUAL, NOT_EQUAL, LESS_THAN, GREATER_THAN, LESS_EQUAL, GREATER_EQUAL, PLUS, MINUS}))
+        return true;
+    return false;
+}
+
+bool unary()
+{
+    if (check({NOT}))
+        return terminal(NOT) && unary();
+    if (check({MINUS}))
+        return terminal(MINUS) && unary();
+    if (check({IDENTIFIER, PARENTHESIS_OPEN, NUMBER, CHAR_VALUE, STRING_VALUE, TRUE, FALSE}))
+        return factor();
+    return false;
+}
+
+bool factor()
+{
+    if (check({IDENTIFIER}))
+        return terminal(IDENTIFIER) && factorPrime1() && factorPrime2();
+    if (check({NUMBER}))
+        return terminal(NUMBER) && factorPrime2();
+    if (check({CHAR_VALUE}))
+        return terminal(CHAR_VALUE) && factorPrime2();
+    if (check({STRING_VALUE}))
+        return terminal(STRING_VALUE) && factorPrime2();
+    if (check({TRUE}))
+        return terminal(TRUE) && factorPrime2();
+    if (check({FALSE}))
+        return terminal(FALSE) && factorPrime2();
+    if (check({PARENTHESIS_OPEN}))
+        return terminal(PARENTHESIS_OPEN) && expression() && terminal(PARENTHESIS_CLOSE) && factorPrime2();
+    return false;
+}
+
+bool factorPrime1()
+{
+    if (check({PARENTHESIS_OPEN}))
+        return terminal(PARENTHESIS_OPEN) && exprList() && terminal(PARENTHESIS_CLOSE);
+    if (check({PARENTHESIS_CLOSE, BRACKET_OPEN, BRACKET_CLOSE, COMMA, SEMICOLON, ASSIGN, OR, AND, EQUAL, NOT_EQUAL, LESS_THAN, GREATER_THAN, LESS_EQUAL, GREATER_EQUAL, PLUS, MINUS, MULTIPLY, DIVIDE, MODULO}))
+        return true;
+    return false;
+}
+
+bool factorPrime2()
+{
+    if (check({BRACKET_OPEN}))
+        return terminal(BRACE_OPEN) && expression() && terminal(BRACE_CLOSE) && factorPrime2();
+    if (check({PARENTHESIS_CLOSE, BRACKET_CLOSE, COMMA, SEMICOLON, ASSIGN, OR, AND, EQUAL, NOT_EQUAL, LESS_THAN, GREATER_THAN, LESS_EQUAL, GREATER_EQUAL, PLUS, MINUS, MULTIPLY, DIVIDE, MODULO}))
+        return true;
+    return false;
+}
+
+int main()
+{
+    tokens = {INTEGER, IDENTIFIER, ASSIGN, NUMBER, SEMICOLON, NUMBER, PLUS, NUMBER, SEMICOLON, END_OF_FILE};
+    std::cout << program() << '\n';
+    tokens = {VOID, IDENTIFIER, PARENTHESIS_OPEN, INTEGER, IDENTIFIER, PARENTHESIS_CLOSE, BRACE_OPEN, NUMBER, ASSIGN, NUMBER, SEMICOLON, BRACE_CLOSE, END_OF_FILE};
+    curr = 0;
+    std::cout << program() << '\n';
 }
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include "Scanner.h"
+#include "Parser.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+	{
+		std::cout << "No source file provided" << std::endl;
+		return 1;
+	}
+	
+    const std::string filePath = argv[1];
+    std::ifstream file(filePath, std::ios::binary | std::ios::ate);
+
+    if (!file) {
+        std::cerr << "Unable to open file: " << filePath << std::endl;
+        return 1;
+    }
+
+    std::streamsize fileSize = file.tellg();
+
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0, std::ios::beg);
+
+    if (file.read(buffer.data(), fileSize)) {
+        std::cout << "File loaded successfully." << std::endl;
+    }
+    else {
+        std::cerr << "Error reading the file." << std::endl;
+    }
+
+    file.close();
+
+    char* pos = &buffer[0];
+    std::vector<Token> tokens;
+    std::vector<std::string> values;
+
+    std::cout << "INFO SCAN - Start scanning...\n";
+
+    while (pos <= &buffer[buffer.size()-1]) {
+        gettoken(pos,tokens,values, &buffer[buffer.size()-1]);
+    }
+    tokens.push_back(END_OF_FILE);
+    
+    std::cout << "INFO SCAN - Completed with "<<errorCounter<<" errors\n";
+}
